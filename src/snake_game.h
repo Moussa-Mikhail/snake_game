26bd86af42a17e9613587691357e9af22d33f6d1
Moussa Mikhail <moussamikhail56@gmail.com>
2022-07-17 20:48:51 +0200

    rename methods

--- src/snake_game.h
+++ src/snake_game.h
@@ -8,19 +8,23 @@
 
 class SnakeGame {
    public:
+    const static int MAX_X = 15;
+
+    const static int MAX_Y = 15;
+
+    const static int INITIAL_LENGTH = 3;
+
     SnakeGame();
 
-    void update(VelDirection);
+    void update(std::optional<VelDir> dir);
 
     int get_score() const;
 
-    const static int MAX_X = 17;
-
-    const static int MAX_Y = 17;
-
-    void time_step();
-
     bool has_collided() const;
+
+    bool has_collided_with_walls() const;
+
+    bool has_collided_with_tail() const;
 
    private:
     Snake snake;
