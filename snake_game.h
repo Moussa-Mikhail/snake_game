f27f33ad7328d1bd6b88242a8cd9ef2040c3361f
Moussa Mikhail <moussamikhail56@gmail.com>
2022-09-05 23:00:05 -0500

    add pos getters, let width height and length be parameters

--- snake_game.h
+++ snake_game.h
@@ -8,17 +8,33 @@
 
 class SnakeGame {
    public:
-    const static int WIDTH = 15;
+    const int WIDTH;
 
-    const static int HEIGHT = 15;
+    const int HEIGHT;
 
-    const static int INITIAL_LENGTH = 3;
+    const int INITIAL_LENGTH = 3;
 
-    SnakeGame();
+    SnakeGame(int width = 15, int height = 15, int length = 3) : WIDTH(width), HEIGHT(height), INITIAL_LENGTH(length) {
+        int distance_from_center = 4;
+
+        int init_x = WIDTH / 2 - distance_from_center;
+
+        int init_y = HEIGHT / 2;
+
+        snake = Snake(init_x, init_y, INITIAL_LENGTH);
+
+        fruit = Fruit(init_x + 2 * distance_from_center, init_y);
+    };
 
     void update(std::optional<VelDir> dir);
 
     int get_score() const;
+
+    Pos get_head_pos() const;
+
+    std::vector<Pos> get_tail_pos() const;
+
+    Pos get_fruit_pos() const;
 
     bool has_collided() const;
 
