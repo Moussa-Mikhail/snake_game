03fc242e36ca95798edf943da930f0e2b9182b1f
Moussa Mikhail <moussamikhail56@gmail.com>
2022-09-05 22:52:32 -0500

    make display hold reference to game

--- display.h
+++ display.h
@@ -2,30 +2,42 @@
 #define SNAKE_DISPLAY_H
 
 #include "display_interface.h"
+#include "snake_game.h"
 
 class Display : public DisplayInterface {
    public:
-    Display(int width, int height) : width(width), height(height){};
+    Display(const SnakeGame& game) : game{game}, WIDTH(game.WIDTH), HEIGHT(game.HEIGHT){};
+
+    const SnakeGame& game;
+
+    const int WIDTH;
+
+    const int HEIGHT;
 
     void draw_walls() override;
 
-    void draw_snake(const Pos head_pos, const std::vector<Pos> tail_pos) override;
+    void draw_snake() override;
 
-    void draw_fruit(const Pos fruit_pos) override;
+    void draw_fruit() override;
 
-    void draw_score(int score) override;
+    void draw_score() override;
+
+    void draw_game() override;
 
     void draw_game_over() override;
 
     void draw_welcome_message() override;
 
-    void draw_game_over_message() override;
+    void clear_screen(char fill = ' ');
 
-   private:
-    const int width;
-    const int height;
+    // draw symbols
+    const static char WALL = 'W';
 
-    const static char wall_char = '#';
+    const static char HEAD = 'H';
+
+    const static char TAIL = 'T';
+
+    const static char FRUIT = 'F';
 };
 
 #endif
