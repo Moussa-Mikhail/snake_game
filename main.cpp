bb83ffd9bdecbb67e1bfff07f6490a6779cdc83c
Moussa Mikhail <moussamikhail56@gmail.com>
2022-09-05 23:01:01 -0500

    basic working logic

--- main.cpp
+++ main.cpp
@@ -1,40 +1,74 @@
+#include <math.h>
 #include <windows.h>
 
+#include <chrono>
 #include <iostream>
 #include <string>
+#include <thread>
 
-// #include "snake_game.h"
+#include "display.h"
+#include "display_interface.h"
+#include "snake_game.h"
 
-void setCursorPosition(int x, int y);
+std::optional<VelDir> get_dir();
 
-void clear_screen(char fill = ' ');
+const int UPDATES_PER_SECOND = 1;
+
+const int MILLISECS_PER_SECONDS = std::pow(10, 3);
+
+const auto MILLISECS_PER_UPDATES = std::chrono::milliseconds(MILLISECS_PER_SECONDS / UPDATES_PER_SECOND);
 
 int main() {
+    SnakeGame game;
+
+    DisplayInterface *base_display = &Display(game);
+
+    base_display->clear_screen();
+
+    base_display->draw_walls();
+
+    base_display->draw_welcome_message();
+
+    std::cin.ignore(1);
+
+    auto now = std::chrono::steady_clock::now();
+
+    auto last_time = now;
+
+    while (!game.has_collided()) {
+        now = std::chrono::steady_clock::now();
+
+        auto delta_time = std::chrono::duration_cast<std::chrono::milliseconds>(now - last_time);
+
+        last_time = now;
+
+        if (delta_time < MILLISECS_PER_UPDATES) {
+            std::this_thread::sleep_for(MILLISECS_PER_UPDATES - delta_time);
+        }
+
+        base_display->draw_game();
+
+        std::optional<VelDir> dir = get_dir();
+
+        game.update(dir);
+    }
+
+    base_display->draw_game_over();
+
+    std::cin.ignore(1);
+
+    base_display->clear_screen();
 }
 
-void setCursorPosition(int x, int y) {
-    static const HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
-    std::cout.flush();
-    COORD coord = {(SHORT)x, (SHORT)y};
-    SetConsoleCursorPosition(hOut, coord);
-}
-
-void clear_screen(char fill) {
-    COORD tl = {0, 0};
-    CONSOLE_SCREEN_BUFFER_INFO s;
-    HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
-    GetConsoleScreenBufferInfo(console, &s);
-    DWORD written, cells = s.dwSize.X * s.dwSize.Y;
-    FillConsoleOutputCharacter(console, fill, cells, tl, &written);
-    FillConsoleOutputAttribute(console, s.wAttributes, cells, tl, &written);
-    SetConsoleCursorPosition(console, tl);
-}
-
-template <typename T>
-void setCursorPosition(T obj) {
-    int x = obj.pos.x;
-
-    int y = obj.pos.y;
-
-    setCursorPosition(x, y);
+std::optional<VelDir> get_dir() {
+    if (GetAsyncKeyState(VK_UP)) {
+        return VelDir::Up;
+    } else if (GetAsyncKeyState(VK_DOWN)) {
+        return VelDir::Down;
+    } else if (GetAsyncKeyState(VK_LEFT)) {
+        return VelDir::Left;
+    } else if (GetAsyncKeyState(VK_RIGHT)) {
+        return VelDir::Right;
+    }
+    return std::nullopt;
 }
